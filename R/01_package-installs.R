2f95dc49f1f7227058723a2dd329667372b11797
Shane B Galvin <113721219@umail.ucc.ie>
2021-03-15 07:51:37 +0000

    extra comments

--- R/01_package-installs.R
+++ R/01_package-installs.R
@@ -2,21 +2,21 @@
 
 # Instructions ------------------------------------------------------------
 # (1)
-#   Uncomment each line (7, 8, 9)
+#   Uncomment each line (14:18)
 #   Run the lines of code to install the packages
 #   Then insert the comment again -> '#'
 
 # (2)
-#   uncomment line 19
+#   uncomment line 22
 #   Run the line of code to install psketti
 
 # (1)  Run each line once
-#install.packages("tidyverse", dependencies = TRUE)
-#install.packages("viridis")
-#install.packages("devtools", dependencies = TRUE)
-#install.packages("eRm", dependencies = TRUE)
-#install.packages("RColorBrewer")  # colour palettes
+#install.packages("tidyverse", dependencies = TRUE)   # Data managment/cleaning/preparation/plotting etc.
+#install.packages("viridis")                          # viridis color palettes
+#install.packages("devtools", dependencies = TRUE)    # Extra R utilities
+#install.packages("eRm", dependencies = TRUE)         # Rasch models and CML estimation
+#install.packages("RColorBrewer")                     # colour palettes
 
 
 # (2) install psketti
-#devtools::install_github('SBGalvin/psketti')
+#devtools::install_github('SBGalvin/psketti')         # Package for investigatory Rasch Models ## use with eRm
